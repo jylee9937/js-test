3d54c1c754f0ce7e01b4ed52ecaa18983a124601
jylee9937 <lkj49864@ajou.ac.kr>
2021-08-22 19:22:34 +0900

    상속(확장)

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -118,34 +118,72 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+var Vehicle = function Vehicle(name, wheel) {
+  _classCallCheck(this, Vehicle);
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+  this.name = name;
+  this.wheel = wheel;
+};
 
-// ES6 Classes
-var User = /*#__PURE__*/function () {
-  function User(first, last) {
-    _classCallCheck(this, User);
+var myVehicle = new Vehicle('운송수단', 2);
+console.log(myVehicle);
 
-    this.firstName = first;
-    this.lastName = last;
+var Bicycle = /*#__PURE__*/function (_Vehicle) {
+  _inherits(Bicycle, _Vehicle);
+
+  var _super = _createSuper(Bicycle);
+
+  function Bicycle(name, wheel) {
+    _classCallCheck(this, Bicycle);
+
+    return _super.call(this, name, wheel);
   }
 
-  _createClass(User, [{
-    key: "getFullName",
-    value: function getFullName() {
-      return "".concat(this.firstName, " ").concat(this.lastName);
-    }
-  }]);
+  return Bicycle;
+}(Vehicle);
 
-  return User;
-}();
+var myBicycle = new Bicycle('삼천리', 2);
+var daughtersBicycle = new Bicycle('세발', 3);
+console.log(myBicycle);
 
-var heropy = new User('Heropy', 'Park');
-console.log(heropy);
-console.log(heropy.getFullName());
+var Car = /*#__PURE__*/function (_Vehicle2) {
+  _inherits(Car, _Vehicle2);
+
+  var _super2 = _createSuper(Car);
+
+  function Car(name, wheel, license) {
+    var _this;
+
+    _classCallCheck(this, Car);
+
+    _this = _super2.call(this, name, wheel);
+    _this.license = license;
+    return _this;
+  }
+
+  return Car;
+}(Vehicle);
+
+var myCar = new Car('벤츠', 4, true);
+console.log(myCar);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -174,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52138" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58512" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
