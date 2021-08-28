936ae06aa10f02b4ddc5cdadf8e8f80d060008d6
jylee9937 <lkj49864@ajou.ac.kr>
2021-08-28 16:02:44 +0900

    OMDb Api

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-const user = {
-  name: 'HEROPY',
-  age: 85,
-  emails: [
-    'thesecon@gmail.com',
-    'neo@zillinks.com'
-  ]
+// 구글 검색창에 omdbapi 검색
+// api주소 활용시에 알아야 되는 개념으로 Query String이 나옴
+// 주소?속성=값&속성=값&속성=값      이러한 형태
+// axios 구글에 검색
+// 부라우저에서 HTTP 요청 처리를 위해 설치하는 것이라고 한다.
+
+// Uncaught SyntaxError: Unexpected token < 문제가 발생해서 다음과 같은 페이지를 참고해서 해결함 http://lab.naminsik.com/4011
+
+import axios from 'axios'
+
+function fetchMovies() {
+  axios
+    .get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen') // 데이터를 요청하고 axios에서 데이터를 처리하고 우리가 사용할 수 있게 되는 것이다.
+    .then(res => {
+      console.log(res)
+      const h1El = document.querySelector('h1')
+      const imgEl = document.querySelector('img')
+      h1El.textContent = res.data.Search[0].Title
+      imgEl.src = res.data.Search[0].Poster
+    })
 }
-
-// localStorage는 개발자도구의 어플리케이션탭에서 확인할 수 있음
-// localStorage MDN 구글에 검색하기
-// localStorage VS Session Storage
-
-localStorage.setItem('user', JSON.stringify(user))
-// JSON 형태로 저장함
-// 모든 데이터를 string타입으로 저장해야 의도한대로 저장됨
-console.log(JSON.parse(localStorage.getItem('user')))
-// localStorage.removeItem('user') // 삭제하기
-
-// 수정하고 싶다면?
-const str = localStorage.getItem('user')
-const obj = JSON.parse(str)
-obj.age = 22
-console.log(obj)
-localStorage.setItem('user', JSON.stringify(obj))
-
-// Lodash를 이용해 만들어진 Lowdb를 활용하면 이렇게 원시적인 방법이 아니게 구현할 수 있다.
+// method chaining
+fetchMovies()
